f8a2c894cb99849ae38e23eb20827dacaa78e462
ksmink1 <6022170@mborijnland.nl>
2023-10-04 13:07:58 +0200

    added register pages skeleton

--- register.php
+++ register.php
@@ -5,20 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+
+<!-- Include the header -->
 <?php include('header.php'); ?>
 <br>
+
 <div class="login-form">
-    <h2>Login</h2>
-    <form action="" method="POST">
+    <h2>Register</h2>
+    <form action="register.php" method="POST">
         <div class="mb-3">
             <input type="text" class="form-control" name="username" placeholder="Username" required>
         </div>
         <div class="mb-3">
             <input type="password" class="form-control" name="password" placeholder="Password" required>
         </div>
-        <button type="submit" class="btn btn-primary">Login</button>
+        <div class="mb-3">
+            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required>
+        </div>
+        <div class="mb-3">
+            <input type="email" class="form-control" name="email" placeholder="Email" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Register</button>
     </form>
-    <p>Don't have an account? <a href="register.php">Register</a></p>
+    <p>Already have an account? <a href="login.php">Login</a></p>
 </div>
+
+
 </body>
 </html>
