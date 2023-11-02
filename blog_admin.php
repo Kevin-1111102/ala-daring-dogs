a8f263d49069247009aade1923320ea4322984bd
ksmink1 <6022170@mborijnland.nl>
2023-11-02 13:54:00 +0100

    ckeditor toegevoegd omdat ik te lui bent om de enter issue te fixen

--- blog_admin.php
+++ blog_admin.php
@@ -1,5 +1,4 @@
 <?php
-// Include the header.php file
 require_once('header.php');
 
 if (isset($_GET['id'])) {
@@ -11,38 +10,41 @@ if (isset($_GET['id'])) {
     $article_id = $_GET['id'];
 
     if ($data) {
-        // Separate the image URLs
         $imageUrls = explode(', ', $data['images']);
 
-        // Output HTML for displaying the blog post
-
-        if(isset($_POST['edit_submit'])){
+        if (isset($_POST['edit_submit'])) {
             $stmt = $pdo->prepare("UPDATE blog SET `title`='" . $_POST['edit_title'] . "', `text`='" . $_POST['edit_text'] . "' WHERE id = '$article_id'");
-            $stmt->execute();  
-        }
+            $stmt->execute();
 
+            // Redirect to admin.php
+            header('Location: admin.php');
+            exit; // Ensure that no further code execution happens after the redirect
+        }
         ?>
         <body>
             <form class="edit" action="" method="POST">
                 <?php if (!empty($imageUrls[0])) : ?>
-                    <img class="blog_image image1" alt="Failed to load image" src="<?php echo $imageUrls[0]; ?>"/>
+                    <img class="blog_image image1" alt="Failed to load image" src="<?php echo $imageUrls[0]; ?>" />
                 <?php endif; ?>
 
                 <input type="text" name="edit_title" class="edit_title" value="<?php echo $data['title']; ?>" />
-                <textarea name='edit_text' class="edit_text" ><?php echo $data['text']; ?></textarea> 
+                <textarea name='edit_text' class="edit_text"><?php echo $data['text']; ?></textarea>
                 <input type="submit" name="edit_submit" class="edit_submit" value="Opslaan" />
                 <?php if (!empty($imageUrls[1])) : ?>
-                    <div><img class="blog_image image2" alt="Failed to load image"src="<?php echo $imageUrls[1]; ?>"/></div>
+                    <div><img class="blog_image image2" alt="Failed to load image" src="<?php echo $imageUrls[1]; ?>" /></div>
                 <?php endif; ?>
             </form>
         </body>
     <?php
     } else {
-        // Handle the case where no blog post with the specified ID was found
         echo "Blog post not found.";
     }
 } else {
-    // Handle the case where 'id' parameter is not set
     echo "Invalid blog post ID.";
 }
 ?>
+<script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+
+<script>
+    CKEDITOR.replace('edit_text');
+</script>
--- create.php
+++ create.php
@@ -2,27 +2,32 @@
     // Include the header.php file
     require_once('header.php');
 
-
     if(isset($_POST['edit_submit'])){
-        $images = $_POST['edit_image1'] . "," . $_POST['edit_image2'];
+        $images = $_POST['edit_image1'] . ", " . $_POST['edit_image2'];
         $stmt = $pdo->prepare("INSERT INTO `blog`(`id`, `user_id`, `title`, `text`, `images`) VALUES (NULL, :user_id, :edit_title, :edit_text, :images)");
 
         $stmt->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
         $stmt->bindParam(':edit_title', $_POST['edit_title'], PDO::PARAM_STR);
         $stmt->bindParam(':edit_text', $_POST['edit_text'], PDO::PARAM_STR);
         $stmt->bindParam(':images', $images, PDO::PARAM_STR);
-        
+
         $stmt->execute();
-        
     }
 ?>
+
 <body>
     <form class="edit" action="" method="POST">
         <input type="text" name="edit_title" class="edit_title"/>
-        <textarea name='edit_text' class="edit_text" ></textarea> 
+        <textarea name='edit_text' id="edit_text" class="edit_text"></textarea> <!-- Add an 'id' to the textarea -->
         <input type="text" name="edit_image1" />
-        <input type="text" name="edit_image2" />
+        <input type="text" name "edit_image2" />
         <input type="submit" name="edit_submit" class="edit_submit" value="Opslaan" />
     </form>
 </body>
 
+<script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script> <!-- Include CKEditor from CDN -->
+
+<script>
+    // Replace the textarea with CKEditor
+    CKEDITOR.replace('edit_text');
+</script>
